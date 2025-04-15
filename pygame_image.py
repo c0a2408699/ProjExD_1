6cd9a3e2465606e75830576ca3bc85df9b34723b
c0a2408699 <c0a2408699@edu.teu.ac.jp>
2025-04-15 16:18:00 +0900

    無限背景

--- pygame_image.py
+++ pygame_image.py
@@ -9,26 +9,29 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img1 = pg.image.load("fig/pg_bg.jpg")
-    bg_img1=pg.transform.flip(bg_img1,True,False)
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img3 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2=pg.transform.flip(bg_img2,True,False)
     kk=pg.image.load("fig/3.png")
     kk=pg.transform.flip(kk,True,False)
-   
+    
     tmr = 0
-    t=300
+         
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        screen.blit(bg_img, [tmr, 0])
-        screen.blit(bg_img1, [tmr+1600, 0])  
-        screen.blit(kk, [t, 200])
+        tmr=tmr%3200
+        screen.blit(bg_img1, [-tmr, 0])
+        screen.blit(bg_img2, [-tmr+1600, 0])  
+        screen.blit(bg_img3, [-tmr+3200, 0])  
+        screen.blit(kk, [300, 200])
         pg.display.update()
-        tmr -= 1
-        t+=0.1
-                  
-        clock.tick(200)
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_SPACE]: 
+             kk.move_ip((0, -1))
+        tmr += 1
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
